ac9c0a126b44928140452dd9a18829298f984d72
CedricFriscia <cedricfriscia@gmail.com>
2023-11-20 11:44:36 +0100

    change data from back end to json

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -16,75 +16,75 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const ExpandMore = styled((props) => {
-   const { expand, ...other } = props;
-   return <IconButton {...other} />;
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-   marginLeft: "auto",
-   transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-   }),
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
 }));
 
 export default function RecipeReviewCard({
-   firstname,
-   lastname,
-   birthday,
-   search,
-   linkedin,
-   description,
+  firstname,
+  lastname,
+  birthday,
+  search,
+  linkedin,
+  description,
 }) {
-   const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = React.useState(false);
 
-   const handleExpandClick = () => {
-      setExpanded(!expanded);
-   };
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-   return (
-      <Card sx={{ width: 345 }}>
-         <CardHeader
-            avatar={
-               <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
-                  CF
-               </Avatar>
-            }
-            action={
-               <IconButton aria-label='settings'>
-                  <MoreVertIcon />
-               </IconButton>
-            }
-            title={firstname + " " + lastname}
-            subheader={birthday}
-         />
+  return (
+    <Card sx={{ width: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            CF
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={firstname + " " + lastname}
+        subheader={birthday}
+      />
 
-         <CardContent>
-            <Typography variant='body2' color='text.secondary'>
-               {search}
-            </Typography>
-         </CardContent>
-         <CardActions disableSpacing>
-            <IconButton aria-label='add to favorites'>
-               <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label='share'>
-               <ShareIcon />
-            </IconButton>
-            <ExpandMore
-               expand={expanded}
-               onClick={handleExpandClick}
-               aria-expanded={expanded}
-               aria-label='show more'
-            >
-               <ExpandMoreIcon />
-            </ExpandMore>
-         </CardActions>
-         <Collapse in={expanded} timeout='auto' unmountOnExit>
-            <CardContent>
-               <Typography paragraph>Présentation:</Typography>
-               <Typography paragraph>{description}</Typography>
-               <Typography paragraph>{linkedin}</Typography>
-            </CardContent>
-         </Collapse>
-      </Card>
-   );
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {search}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Description :</Typography>
+          <Typography paragraph>{description}</Typography>
+          <Typography paragraph>{linkedin}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
--- src/Pages/Jeune/Jeune.jsx
+++ src/Pages/Jeune/Jeune.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 // API import
 import api from "../../api/api";
@@ -11,73 +12,76 @@ import Card from "../../Components/Card/Card";
 import "./styles.scss";
 
 const Jeune = () => {
-   const [candidats, setCandidats] = useState([]);
-   const [finder, setFinder] = useState("");
+  const [candidats, setCandidats] = useState([]);
+  const [finder, setFinder] = useState("");
 
-   useEffect(() => {
-      api.get("/candidats")
-         .then((res) => {
-            setCandidats(res.data);
-         })
-         .catch((err) => console.log("ERREUR", err));
-   }, []);
+  // ...
 
-   const onChange = (event) => {
-      setFinder(event.target.value);
-   };
+  useEffect(() => {
+    axios
+      .get("src/data/candidats.json")
+      .then((res) => {
+        console.log("Réponse JSON :", res.data);
+        setCandidats(res.data.candidats); // Assurez-vous que la structure est correcte ici
+      })
+      .catch((err) => console.log("ERREUR", err));
+  }, []);
 
-   return (
-      <div className='jeune'>
-         <section className='jeune__search'>
-            <div className='input-container'>
-               <input
-                  type='text'
-                  name='text'
-                  className='input'
-                  placeholder='Recherchez un candidat'
-                  value={finder}
-                  onChange={onChange}
-               />
-               <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 24 24'
-                  className='icon'
-               >
-                  <rect fill='white' height='24' width='24'></rect>
-                  <path
-                     fill=''
-                     d='M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM9 11.5C9 10.1193 10.1193 9 11.5 9C12.8807 9 14 10.1193 14 11.5C14 12.8807 12.8807 14 11.5 14C10.1193 14 9 12.8807 9 11.5ZM11.5 7C9.01472 7 7 9.01472 7 11.5C7 13.9853 9.01472 16 11.5 16C12.3805 16 13.202 15.7471 13.8957 15.31L15.2929 16.7071C15.6834 17.0976 16.3166 17.0976 16.7071 16.7071C17.0976 16.3166 17.0976 15.6834 16.7071 15.2929L15.31 13.8957C15.7471 13.202 16 12.3805 16 11.5C16 9.01472 13.9853 7 11.5 7Z'
-                     clipRule='evenodd'
-                     fillRule='evenodd'
-                  ></path>
-               </svg>
-            </div>
-         </section>
-         <Link className='jeune__button' to={"/candidat/create"}>
-            Mon profil
-         </Link>
+  // ...
 
-         <div className='card__container'>
-            {candidats
-               .filter((candidat) =>
-                  `${candidat.firstname} ${candidat.lastname}`
-                     .toLowerCase()
-                     .includes(finder.toLowerCase())
-               )
-               .map((candidat) => (
-                  <Card
-                     key={candidat.id}
-                     firstname={candidat.firstname}
-                     lastname={candidat.lastname}
-                     birthday={candidat.birthday}
-                     search={candidat.search}
-                     linkedin={candidat.linkedin}
-                     description={candidat.description}
-                  />
-               ))}
-         </div>
+  const onChange = (event) => {
+    setFinder(event.target.value);
+  };
+
+  return (
+    <div className="jeune">
+      <section className="jeune__search">
+        <div className="input-container">
+          <input
+            type="text"
+            name="text"
+            className="input"
+            placeholder="Recherchez un candidat"
+            value={finder}
+            onChange={onChange}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="icon"
+          >
+            <rect fill="white" height="24" width="24"></rect>
+            <path
+              fill=""
+              d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM9 11.5C9 10.1193 10.1193 9 11.5 9C12.8807 9 14 10.1193 14 11.5C14 12.8807 12.8807 14 11.5 14C10.1193 14 9 12.8807 9 11.5ZM11.5 7C9.01472 7 7 9.01472 7 11.5C7 13.9853 9.01472 16 11.5 16C12.3805 16 13.202 15.7471 13.8957 15.31L15.2929 16.7071C15.6834 17.0976 16.3166 17.0976 16.7071 16.7071C17.0976 16.3166 17.0976 15.6834 16.7071 15.2929L15.31 13.8957C15.7471 13.202 16 12.3805 16 11.5C16 9.01472 13.9853 7 11.5 7Z"
+              clipRule="evenodd"
+              fillRule="evenodd"
+            ></path>
+          </svg>
+        </div>
+      </section>
+
+      <div className="card__container">
+        {candidats
+          .filter((candidat) =>
+            `${candidat.prenom} ${candidat.nom}`
+              .toLowerCase()
+              .includes(finder.toLowerCase())
+          )
+          .map((candidat) => (
+            <Card
+              key={candidat.id}
+              firstname={candidat.prenom}
+              lastname={candidat.nom}
+              birthday={candidat.anniversaire}
+              search={candidat.recherche}
+              linkedin={candidat.linkedin}
+              description={candidat.description}
+            />
+          ))}
       </div>
-   );
+    </div>
+  );
 };
 
 export default Jeune;
