8741c7f16449e3add6b6b645985e95dfc1b0e603
CedricFriscia <cedricfriscia@gmail.com>
2023-11-20 12:42:17 +0100

    change on linkedin display

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
@@ -10,10 +9,12 @@ import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
+
+import "./styles.scss";
+
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -35,17 +36,24 @@ export default function RecipeReviewCard({
   description,
 }) {
   const [expanded, setExpanded] = React.useState(false);
+  const [link, setLink] = React.useState(true);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
+  const handleLinkedInClick = () => {
+    window.open(linkedin, "_blank");
+  };
+
   return (
-    <Card sx={{ width: 345 }}>
+    <Card sx={{ width: 345 }} className="card">
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            CF
+            {firstname && lastname
+              ? `${firstname.charAt(0)}${lastname.charAt(0)}`
+              : ""}
           </Avatar>
         }
         action={
@@ -63,12 +71,12 @@ export default function RecipeReviewCard({
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
+        {linkedin === "" ? null : (
+          <IconButton aria-label="Linkedin" onClick={handleLinkedInClick}>
+            <LinkedInIcon />
+          </IconButton>
+        )}
+
         <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
